53b9f4239234d629e4471a2398050369f2188c1c
William Lupaca Ticona <wlupacat@gmail.com>
2024-09-09 01:41:18 -0500

    feat: add buttons component

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Buttons() {
+  return (
+    <div className="order-summary__buttons">
+      <a href="#" className="order-summary__proceed-btn">Proceed to Payment</a>
+      <a href="#" className="order-summary__cancel-btn">Cancel Order</a>
+    </div>
+  );
+}
+
+export default Buttons;
