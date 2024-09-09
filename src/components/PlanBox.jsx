cce11bdee2d9c0fd6bdcaa086b0dbfb62f64246c
William Lupaca Ticona <wlupacat@gmail.com>
2024-09-09 01:55:00 -0500

    feat: update plan box component

--- src/components/PlanBox.jsx
+++ src/components/PlanBox.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-import iconMusic from "../assets/images/icon-music.svg";
+import PlanDetails from "./PlanDetails";
+import ChangeLink from "./ChangeLink";
 
 function PlanBox() {
   return (
     <div className="order-summary__plan-box">
-      <div className="order-summary__plan-box-left">
-        <img src={iconMusic} alt="Music Icon" className="order-summary__plan-box-icon" />
-        <div>
-          <h5 className="order-summary__plan-title">Annual Plan</h5>
-          <p className="order-summary__plan-price">$59.99/year</p>
-        </div>
-      </div>
-      <a href="#" className="order-summary__plan-change">Change</a>
+      <PlanDetails />
+      <ChangeLink />
     </div>
   );
 }
 
 export default PlanBox;
+
