f643aa4700b6d358c3d01270a5ed9442e8b957eb
William Lupaca Ticona <wlupacat@gmail.com>
2024-09-09 01:53:45 -0500

    feat: add hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import heroImage from "../assets/images/illustration-hero.svg";
 
 function Hero() {
-    return (
-      <div className="order-summary__hero">
-        <img src={heroImage} alt="Hero" className="order-summary__hero-image" />
-      </div>
-    );
-  }
-  
-  export default Hero;
+  return (
+    <div className="order-summary__hero">
+      <img src={heroImage} alt="Hero" className="order-summary__hero-image" />
+    </div>
+  );
+}
+
+export default Hero;
